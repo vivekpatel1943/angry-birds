ea1e8ec8d9eabd3a3d241f3355baf64f4f11b8d7
bittu1943 <136315448+bittu1943@users.noreply.github.com>
2024-12-01 18:34:36 +0530

    now the bird flies

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ ctx.fillStyle = 'green';
 // Add a rectangle at (10,10) with size 100*100 pixels
 let rectangle = {
     x : 10,
-    y: 10,
-    width: 100,
-    height : 100
+    y: 200,
+    width: 20,
+    height : 20
 }
 ctx.fillRect(rectangle.x,rectangle.y,rectangle.width,rectangle.height)
 console.log(rectangle)
@@ -14,27 +14,26 @@ console.log(rectangle)
 document.addEventListener('keydown',(event) => {
     ctx.clearRect(0,0,canvas.width,canvas.height);
     if(event.key === "ArrowRight"){
-        if((rectangle.x + 130) < canvas.width){
-            rectangle.x = rectangle.x + 50;  
+        if((rectangle.x + 10) < canvas.width){
+            rectangle.x = rectangle.x + 10;  
         }      
     }
     
     if(event.key === "ArrowLeft"){   
-        if(rectangle.x  > 20){
-            rectangle.x = rectangle.x - 50;  
+        if(rectangle.x  > (rectangle.width + 10)){
+            rectangle.x = rectangle.x - 10;  
         }
-        
     }
 
     if(event.key === "ArrowUp"){
-        if(rectangle.y  > 50){
-            rectangle.y = rectangle.y - 50;
+        if(rectangle.y   > (rectangle.height + 10)){
+            rectangle.y = rectangle.y - 10;
         }       
     }
 
     if(event.key === "ArrowDown"){
-        if(rectangle.y + 120 < canvas.height){
-            rectangle.y = rectangle.y + 50;  
+        if((rectangle.y + 10) < canvas.height){
+            rectangle.y = rectangle.y + 10;  
         }
     }
         
@@ -42,4 +41,25 @@ document.addEventListener('keydown',(event) => {
     console.log(rectangle)
 })
 
+
+function flyRight(){
+    const intervalId = setInterval(() => {
+        if(rectangle.x + rectangle.width < canvas.width){
+            rectangle.x += 5;
+            ctx.clearRect(0,0,canvas.width,canvas.height)
+            ctx.fillRect(rectangle.x,rectangle.y,rectangle.width,rectangle.height)
+        }else{
+            clearInterval(intervalId);
+        }
+       
+    },100)
+}
+
+document.addEventListener('keydown',(event) => {
+    
+       
+        flyRight();
+        
+   
+})
 
